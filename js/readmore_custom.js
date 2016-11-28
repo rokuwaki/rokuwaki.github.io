7310d359a14d8ba8f576ef6290e1bc9fdb3e1875
Ryo Okuwaki <rokuwaki@gmail.com>
2016-11-29 00:59:20 +0900

    Update css, js, index

---
+++ js/readmore_custom.js
@@ -0,0 +1,5 @@
+$('article').readmore({
+    collapsedHeight: 73,
+    moreLink: '<a href="#" style="text-align: center"><span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></a>',
+    lessLink: '<a href="#" style="text-align: center"><span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span></a>',
+});
