051835a0c269c4c449d47fd32e71599dd9a37b1c
Ryo Okuwaki <rokuwaki@gmail.com>
2017-01-26 23:06:08 +0900

    Clear

--- js/readmore_custom.js
+++
@@ -1,5 +0,0 @@
-$('article').readmore({
-    collapsedHeight: 73,
-    moreLink: '<a href="#" style="text-align: center"><i class="fa fa-angle-down"></i></a>',
-    lessLink: '<a href="#" style="text-align: center"><i class="fa fa-angle-up"></i></a>',
-});
