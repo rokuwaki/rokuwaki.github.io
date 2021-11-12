1c5929e07c3a9b0eafa2e0af215612aa099376c1
Ryo Okuwaki <rokuwaki@gmail.com>
2021-11-12 23:45:53 +0000

    rm js

--- geol/js/sortpub.js
+++
@@ -1,8 +0,0 @@
-var options = {
-  valueNames: ['author', 'year', 'pub-title', 'journal']
-};
-var userList = new List('pub-list', options);
-userList.on('searchComplete', function(a) {
-  $("#hits").html('Papers: ' + a.matchingItems.length + '/');
-  $("#total-count").html(userList.size);
-});
